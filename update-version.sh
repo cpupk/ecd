c7edeb8ece50680029b3cc4e8005cf69273efce0
Robert Zenz <Robert.Zenz@bonsaimind.org>
2018-06-28 20:49:34 +0200

    Added a script for updating the version.

---
+++ update-version.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+# This script is used for updating the version of the whole plugin.
+#
+# Usage: ./update-version.sh VERSION
+# Example: ./update-version.sh 3.1.0-SNAPSHOT
+
+# Note that automatically updating the version only works when the versions
+# in the files have not been manually edited and actually match the current
+# version.
+
+
+VERSION="$*"
+
+if [ -z "$VERSION" ]; then
+	echo "update-version.sh VERSION"
+	exit 1
+fi
+
+mvn \
+	org.eclipse.tycho:tycho-versions-plugin:set-version \
+	-DnewVersion="$VERSION"
